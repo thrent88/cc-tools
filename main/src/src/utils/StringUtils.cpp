700a29dcd2f66e57b7e675a1ed1efafc22fc5317
hdx <121@localhost.com>
2025-06-30 17:42:56 +0800

    @Update

--- main/src/src/utils/StringUtils.cpp
+++ main/src/src/utils/StringUtils.cpp
@@ -3,6 +3,7 @@
 //
 
 #include "../../include/cc_tools/utils/StringUtils.h"
+#include <sstream> 
 
 #if _MSVC_LANG >= 201703L || __cplusplus >= 201703L && defined(__has_include)
 // ^ Supports MSVC prior to 15.7 without setting /Zc:__cplusplus to fix __cplusplus
@@ -82,6 +83,35 @@ bool StringUtils::endsWith(const std::string& source, std::string tail) {
 
 bool StringUtils::equal(const std::string& source, const std::string& target) {
     return source.compare(target) == 0;
+}
+
+bool StringUtils::contain(const std::string& string, const std::string& subString) {
+    return string.find(subString) != std::string::npos;
+}
+
+bool StringUtils::isNumber(const std::string& str) {
+    std::stringstream sin(str);  
+	double d;  
+	char c;  
+	if(!(sin >> d))  
+	{
+		/*解释： 
+            sin>>t表示把sin转换成double的变量（其实对于int和float型的都会接收），
+			如果转换成功，则值为非0，如果转换不成功就返回为0 
+        */
+		return false;
+	}
+	if (sin >> c) 
+	{
+		/*解释：
+		此部分用于检测错误输入中，数字加字符串的输入形式（例如：34.f），在上面的的部分（sin>>t）
+		已经接收并转换了输入的数字部分，在stringstream中相应也会把那一部分给清除，
+		此时接收的是.f这部分，所以条件成立，返回false 
+          */ 
+		return false;
+	}  
+	return true;  
+
 }
 
 }  // namespace utils
